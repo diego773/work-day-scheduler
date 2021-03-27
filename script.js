ee426fb2a9793ac7b63e67fec4eb9f7f8bb7ebee
Diego <diegovillarreal123@gmail.com>
2021-03-27 17:24:01 -0500

    new version

--- script.js
+++ script.js
@@ -1,53 +1,21 @@
-// A $( document ).ready() block.
-$(document).ready(function() {
-    $(".saveBtn").on("click", function() {
-    var value = $(this).siblings(".description").val()
-    var time = $(this).parent().attr("id")
-    localStorage.setItem(time, value)
+var responseDate = moment().format("MMMM Do YYYY, h:mm:ss a");
+var dayPlanner = $("#planner");
+var timeHour = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
 
-    })
-// Function hour    
-    function hourUpdater() {
-    var currentHour = moment().hours()
-    $(".time-block").each(function() {
-        var blockHour = parseInt($(this).attr("id").split("-")[1]);
+$(document).ready(function () {
+  $("#todaysDate").append(responseDate);
 
-        if (blockHour < currentHour) {
-            $(this).addClass("past")
-        }
-        else if (blockHour === currentHour) {
-            $(this).removeClass("past")
-            $(this).addClass("present")
-        }else {
-            $(this).removeClass("past")
-            $(this).removeClass("present")
-            $(this).addClass("future")
-        }
-    })
-}
-// Hour Updater
-hourUpdater();
-var interval = setInterval(hourUpdater, 15000)
+  $(".saveBtn").click(function () {
+    $(this).timeHour().attr("id");
+    $(this).dayPlanner("planner").val();
+    localStorage.setItem("hour", "value");
+    console.log(timeHour, dayPlanner);
+  });
 
-// Save to local storage
-$("#hour-9 .description").val(localStorage.getItem("hour-9"))
-$("#hour-10 .description").val(localStorage.getItem("hour-10"))
-$("#hour-11 .description").val(localStorage.getItem("hour-11"))
-$("#hour-12 .description").val(localStorage.getItem("hour-12"))
-$("#hour-13 .description").val(localStorage.getItem("hour-13"))
-$("#hour-14 .description").val(localStorage.getItem("hour-14"))
-$("#hour-15 .description").val(localStorage.getItem("hour-15"))
-$("#hour-16 .description").val(localStorage.getItem("hour-16"))
-$("#hour-17 .description").val(localStorage.getItem("hour-17"))
-
-// Current day calender 
-$("#currentDay").text(moment().format("dddd, MMMM Do"))
-
+  var currentTime = moment().hours();
+  for (i = 0; i < timeHour.length; i++) {
+    if (timeHour < currentTime) {
+      greeting = "Good day";
+    }
+  }
 });
-
-
-
-
-
-
-
